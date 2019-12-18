01a9e61df78280e85b0768cda6af4512654820d6
Denny Pearce <obsidian.y.dragon@gmail.com>
2019-12-18 05:35:52 +0200

    include modules to variables in gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+let gulp         = require('gulp'),
+	sass         = require('gulp-sass'),
+	browserSync  = require('browser-sync'),
+	concat       = require('gulp-concat'),
+	uglify       = require('gulp-uglify-es').default,
+	cleancss     = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	rsync        = require('gulp-rsync'),
+	newer        = require('gulp-newer'),
+	rename       = require('gulp-rename'),
+	responsive   = require('gulp-responsive'),
+    del          = require('del');
+    
