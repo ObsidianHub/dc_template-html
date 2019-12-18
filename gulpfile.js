73b4bdcdf84a47fda375b1cf46d87b89f5fdd8de
Denny Pearce <obsidian.y.dragon@gmail.com>
2019-12-18 05:43:19 +0200

    add task for work with images and clean image

--- gulpfile.js
+++ gulpfile.js
@@ -53,3 +53,33 @@ gulp.task('scripts', function() {
 	.pipe(gulp.dest('app/js'))
 	.pipe(browserSync.reload({ stream: true }))
 });
+
+// Responsive Images
+var quality = 95; // Responsive images quality
+
+// Produce @1x images
+gulp.task('img-responsive-1x', async function() {
+	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('app/img/@1x'))
+		.pipe(responsive({
+			'**/*': { width: '50%', quality: quality }
+		})).on('error', function (e) { console.log(e) })
+		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
+		.pipe(gulp.dest('app/img/@1x'))
+});
+// Produce @2x images
+gulp.task('img-responsive-2x', async function() {
+	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('app/img/@2x'))
+		.pipe(responsive({
+			'**/*': { width: '100%', quality: quality }
+		})).on('error', function (e) { console.log(e) })
+		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
+		.pipe(gulp.dest('app/img/@2x'))
+});
+gulp.task('img', gulp.series('img-responsive-1x', 'img-responsive-2x', bsReload));
+
+// Clean @*x IMG's
+gulp.task('cleanimg', function() {
+	return del(['app/img/@*'], { force: true })
+});
